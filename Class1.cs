e99b2ad910cb321e4bac58bb559cc9c188be691a
daldalux <116430569+daldalux@users.noreply.github.com>
2026-08-29 03:14:36 +0500

    Added the source code of the patches

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DevCheats
+{
+
+    [BepInPlugin("com.myname.repocheat", "Cheats", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            var harmony = new Harmony("com.myname.repocheat");
+            harmony.PatchAll();
+            Logger.LogInfo("Cheats ready");
+        }
+    }
+
+        [HarmonyPatch(typeof(SteamManager), "get_IsDev")]
+    public class PretendDevPatch
+    {
+        [HarmonyPostfix]
+        static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+
+}
